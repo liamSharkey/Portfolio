37e1d229cce44a6802c5e051a90e35f00c789027
liamSharkey <wsharke@uwo.ca>
2022-01-29 13:51:59 -0500

    add new changes -- just updating folder

--- portfolio-project/client/src/App.js
+++ portfolio-project/client/src/App.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from "react";
+import {Button} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CalendarComponent from "./components/CalendarComp";
 
 function App() {
 	return (
 		<div>
-			<h1>Hello world</h1>
-			<h2>lets try this again</h2>
+			<p>Hi again!</p>
+			<CalendarComponent></CalendarComponent>
 		</div>
 	);
 }
---
+++ portfolio-project/client/src/components/CalendarComp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "react-calendar/dist/Calendar.css";
+import Calendar from "react-calendar";
+
+class CalendarComponent extends React.Component {
+	render() {
+		return (
+			<div>
+				<Calendar></Calendar>
+			</div>
+		);
+	}
+}
+
+export default CalendarComponent;
